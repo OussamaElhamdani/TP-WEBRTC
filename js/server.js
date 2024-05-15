69339e3ccb5930db47e993488b4e80914f37ceb8
OussamaElhamdani <oussamahamdani1718@gmail.com>
2024-05-15 13:15:05 -0100

    TP

---
+++ js/server.js
@@ -0,0 +1,56 @@
+var static = require('node-static');
+var http = require('http');
+
+var file = new(static.Server)();
+var app = http.createServer(function (req, res) {
+    file.serve(req, res);
+}).listen(8181);
+
+var io = require('socket.io').listen(app);
+io.sockets.on('connection', function (socket){
+    socket.on('message', function (message) {
+    log('S --> Got message: ', message);
+    socket.broadcast.to(message.channel).emit('message',
+    message.message);
+});
+socket.on('create or join', function (channel) {
+var numClients = io.sockets.clients(channel).length;
+console.log('numclients = ' + numClients);
+if (numClients == 0){
+socket.join(channel);
+socket.emit('created', channel);
+} else if (numClients == 1) {
+io.sockets.in(channel).emit('remotePeerJoining'
+, channel);
+Communication P2P en temps réel sur le web
+Page 11/12
+socket.join(channel);
+socket.broadcast.to(channel).emit('broadcast:
+joined', 'S --> broadcast(): client ' +
+socket.id + ' joined channel ' + channel);
+} else { console.log("Channel full!");
+ socket.emit('full', channel);
+}
+});
+socket.on('response', function (response) {
+log('S --> Got response: ', response);
+socket.broadcast.to(response.channel).emit('response
+',
+response.message);
+});
+socket.on('Bye', function(channel){
+socket.broadcast.to(channel).emit('Bye');
+socket.disconnect();
+});
+socket.on('Ack', function () {
+console.log('Got an Ack!');
+socket.disconnect();
+});
+function log(){
+var array = [">>> "];
+for (var i = 0; i < arguments.length; i++) {
+array.push(arguments[i]);
+}
+socket.emit('log', array);
+}
+});
